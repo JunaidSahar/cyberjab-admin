7bad53974eef6b4012bb58c53371b2c28b2147aa
shaheer-haider <shaheerhaider00000@gmail.com>
2025-10-01 21:22:07 +0500

    Fetch API of user

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       API_BASE_URL: 'https://api.cyberjab.org/',
+      API_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzU5NzYwMzkxLCJpYXQiOjE3NTkxNTU1OTEsImp0aSI6IjE2Yjk3ZWE1ZmIyZjQ1YWQ4MzhjOTE1YjhhYmFkNTUzIiwidXNlcl9pZCI6MiwidG9rZW5fdXVpZCI6IjkxMDgxYzliLTQ0MDQtNDU2Yi05ZGJmLWUwNGFjNmQ4MDk5ZiJ9.MLa3NGLlzxHZYow69vpJp6TtizU2-_VC6w3typANNUk"
     }
   }
 })
